4c422ca1529540603578b0eeeb8c833e0394e29b
junyorff12 <junyorff12@gmail.com>
2019-07-29 23:18:05 -0300

    added jquery to the index.mhl on painel, on home.js

---
+++ js/home.js
@@ -0,0 +1,12 @@
+(function(){
+  'use strict'
+
+  $('.novidades button').click(function() {
+    $('.novidades').toggleClass('painel-aberto');
+  });
+
+  $('.mais-vendidos button').click(function() {
+    $('.mais-vendidos').toggleClass('painel-aberto')
+  });
+
+})()
